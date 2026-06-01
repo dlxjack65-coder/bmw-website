0530e4514af15877a079b6a1753d0ae8dec5f16f
dlxjack65-coder <dlxjack65@gmail.com>
2026-06-01 15:37:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,4 +7,13 @@ document.getElementById("exploreBtn").addEventListener("click", function () {
 document.getElementById("contactForm").addEventListener("submit", function (e) {
     e.preventDefault();
     alert("Thank you! Your message has been sent.");
-});
+});let visits = localStorage.getItem("visits");
+
+if (!visits) {
+    visits = 0;
+}
+
+visits++;
+localStorage.setItem("visits", visits);
+
+document.getElementById("visitor-count").textContent = visits;

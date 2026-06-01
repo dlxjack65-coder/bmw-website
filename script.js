a6c0e5fd226e3dbf3028fef16fc7fb806208866b
dlxjack65-coder <dlxjack65@gmail.com>
2026-06-01 15:50:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,38 @@ visits++;
 localStorage.setItem("visits", visits);
 
 document.getElementById("visitor-count").textContent = visits;
+// Dark / Light Mode
+
+const themeToggle = document.getElementById("themeToggle");
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+});
+
+
+// Live Clock
+
+function updateClock() {
+    const now = new Date();
+
+    document.getElementById("clock").innerHTML =
+        now.toLocaleDateString() + " | " +
+        now.toLocaleTimeString();
+}
+
+setInterval(updateClock, 1000);
+updateClock();
+
+
+// Visitor Counter
+
+let visits = localStorage.getItem("visits");
+
+if (!visits) {
+    visits = 0;
+}
+
+visits++;
+localStorage.setItem("visits", visits);
+
+document.getElementById("visitor-count").textContent = visits;
